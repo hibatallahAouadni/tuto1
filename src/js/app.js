6eb83c13303509ad3582c9d2df12a3782e9ce0c5
Hibatallah Aouadni <hibatallah.aouadni@sifast.com>
2019-04-12 11:51:26 +0100

    CO: Add filter and search methods

--- src/js/app.js
+++ src/js/app.js
@@ -1,7 +1,7 @@
 var app = angular.module("Webmail", [ "ngSanitize", "ngRoute" ]);
 
 /* Webmail Controller */
-app.controller("WebmailCtrl", function($scope, $location, WebmailFactory) {
+app.controller("WebmailCtrl", function($scope, $location, WebmailFactory/*, $filter*/) {
     $scope.dossiers = WebmailFactory.getDossiers();
 
     $scope.dossierCourant = null;
@@ -20,6 +20,36 @@ app.controller("WebmailCtrl", function($scope, $location, WebmailFactory) {
         $scope.emailSelectionne = email;
     }
 
+    /* tri methods */
+    $scope.champTri = null;
+    $scope.triDescendant = false;
+    $scope.triEmails = function(champ) {
+        if($scope.champTri == champ) {
+            $scope.triDescendant = !$scope.triDescendant;
+        } else {
+            $scope.champTri = champ;
+            $scope.triDescendant = false;
+        }
+    }
+    $scope.cssChevronsTri = function(champ) {
+        return { 
+            glyphicon: $scope.champTri == champ,
+            'glyphicon-chevron-up': $scope.champTri == champ && !$scope.triDescendant,
+            'glyphicon-chevron-down': $scope.champTri == champ && $scope.triDescendant 
+        };
+    }
+
+    /* Search method */
+    $scope.query = null;
+    $scope.razSearch = function() {
+        $scope.query = null;
+    }
+
+    /* Filter method --- pas trop efficase car la méthode est appellé multiple fois*/
+    /* $scope.getDossiersFilter = function() {
+        return $filter("filter")($scope.dossierCourant.emails, $scope.query);
+    } */
+
     $scope.$watch(function() {
         return $location.path();
     }, function(newPath) {
@@ -27,7 +57,6 @@ app.controller("WebmailCtrl", function($scope, $location, WebmailFactory) {
         var tabPath = newPath.split("/");
         if(tabPath.length > 1) {
             var valDossier = tabPath[1];
-            console.log("valDossier: " + valDossier);
             $scope.dossiers.forEach(function(item) {
                 if(item.value == valDossier) {
                     $scope.selectionDossier(item);
@@ -44,6 +73,16 @@ app.controller("WebmailCtrl", function($scope, $location, WebmailFactory) {
         }
     });
 
+});
+
+/* Filter */
+app.filter("highlightingSearch", function() {
+    return function(input, query) {
+        if(query) {
+            return input.replace(new RegExp("(" + query + ")", "gi"), "<span class='highlightingSearch'>$1</span>");
+        }
+        return input;
+    }
 });
 
 /* Routes config */
@@ -75,23 +114,23 @@ app.factory('WebmailFactory', function() {
     var factory = {
         dossiers: [
             { value: "RECEPTION", label: 'Boite de réception', emails: [
-                { id:1, from: "Albator", to: "Hiba", subject: "Je reviens", date: "20/03/2019", content: "Je reviens, <b>Je reviens</b>"},
-                { id:2, from: "Loulou", to: "Hiba", subject: "Bisous", date: "18/03/2019", content: "Bisous!!!"},
-                { id:3, from: "Pikatchu", to: "Hiba", subject: "Pika pika!", date: "15/03/2019", content: "Pika pika! Pika pika! Pika pika!"},
-                { id:4, from: "Barbapapa", to: "Hiba", subject: "Hulahup", date: "13/03/2019", content: "Lorem ipsum dolor sit amet!"}
+                { id:1, from: "Albator", to: "Hiba", subject: "Je reviens", date: new Date(2019, 2, 20, 21, 14), content: "Je reviens, <b>Je reviens</b>"},
+                { id:2, from: "Loulou", to: "Hiba", subject: "Bisous", date: new Date(2018, 2, 20, 8, 14), content: "Bisous!!!"},
+                { id:3, from: "Pikatchu", to: "Hiba", subject: "Pika pika!", date: new Date(2019, 5, 12, 21, 14), content: "Pika pika! Pika pika! Pika pika!"},
+                { id:4, from: "Barbapapa", to: "Hiba", subject: "Hulahup", date: new Date(2018, 2, 20, 22, 22), content: "Lorem ipsum dolor sit amet!"}
             ]},
             { value: "ARCHIVES", label: 'Archives', emails: [
-                { id:5, from: "Candy", to: "Hiba", subject: "Bon anniversaire", date: "20/03/2019", content: "Bon anniversaire!!!"},
-                { id:6, from: "Hiro", to: "Hiba", subject: "Konichiwa", date: "18/03/2019", content: "Lorem ipsum dolor sit amet!"},
-                { id:7, from: "Asuka", to: "Hiba", subject: "Ca va chier", date: "20/03/2019", content: "Ca va chier, Ca va chier. Ca va chier."}
+                { id:5, from: "Candy", to: "Hiba", subject: "Bon anniversaire", date: new Date(2019, 2, 20, 21, 14), content: "Bon anniversaire!!!"},
+                { id:6, from: "Hiro", to: "Hiba", subject: "Konichiwa", date: new Date(2018, 7, 18, 20, 20), content: "Lorem ipsum dolor sit amet!"},
+                { id:7, from: "Asuka", to: "Hiba", subject: "Ca va chier", date: new Date(2019, 11, 20, 13, 25), content: "Ca va chier, Ca va chier. Ca va chier."}
             ]},
             { value: "ENVOYES", label: 'Envoyés', emails: [
-                { id:8, from: "Hiba", to: "Albator", subject: "Bien la famille?", date: "20/03/2019", content: "Bien la famille?Bien la famille?Bien la famille?"},
-                { id:9, from: "Hiba", to: "Loulou", subject: "Hulahup Konichiwa", date: "18/03/2019", content: "Hulahup Konichiwa. Hulahup Konichiwa, Hulahup Konichiwa! Hulahup Konichiwa"}
+                { id:8, from: "Hiba", to: "Albator", subject: "Bien la famille?", date: new Date(2019, 0, 20, 14, 15), content: "Bien la famille?Bien la famille?Bien la famille?"},
+                { id:9, from: "Hiba", to: "Loulou", subject: "Hulahup Konichiwa", date: new Date(2019, 2, 17, 22, 11), content: "Hulahup Konichiwa. Hulahup Konichiwa, Hulahup Konichiwa! Hulahup Konichiwa"}
             ]},
             { value: "SPAM", label: 'Courier indésirable', emails: [
-                { id:10, from: "Rue de discount", to: "Hiba", subject: "Envie d'un nouveau frigo?", date: "20/03/2019", content: "Envie d'un nouveau frigo?Envie d'un nouveau frigo?"},
-                { id:11, from: "Sofinnoga", to: "Hiba", subject: "Besoin d'argent?", date: "18/03/2019", content: "Besoin d'argent?Besoin d'argent?Besoin d'argent?"}
+                { id:10, from: "Rue de discount", to: "Hiba", subject: "Envie d'un nouveau frigo?", date: new Date(2019, 2, 20, 7, 14), content: "Envie d'un nouveau frigo?Envie d'un nouveau frigo?"},
+                { id:11, from: "Sofinnoga", to: "Hiba", subject: "Besoin d'argent?", date: new Date(2019, 2, 20, 14, 21), content: "Besoin d'argent?Besoin d'argent?Besoin d'argent?"}
             ]},
         ],
         getDossiers: function() {
